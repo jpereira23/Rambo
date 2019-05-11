bf64f806bb2ac0bb16a721f96d8afd6b36985153
Luis <luee33@gmail.com>
2019-05-11 10:03:04 -0700

    splash screen success

---
+++ Rambo/SplashScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashScreenViewController.swift
+//  Rambo
+//
+//  Created by Waldo-C on 5/11/19.
+//  Copyright © 2019 Jeffery Pereira. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
