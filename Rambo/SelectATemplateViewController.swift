195a87b7eb579c4890d23308fabc3cb9505ecb95
Jeff Pereira <jefferypereira3@gmail.com>
2019-05-14 14:19:25 -0700

    Small gains

--- Rambo/SelectATemplateViewController.swift
+++ Rambo/SelectATemplateViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class SelectATemplateViewController: UIViewController {
+    let coreDataHelper: CoreDataHelper = CoreDataHelper()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if coreDataHelper.loadProfile() != nil {
+            
+        } else {
+            let vc = storyboard?.instantiateViewController(withIdentifier: "onBoarding")
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.window?.rootViewController = vc
+        }
         // Do any additional setup after loading the view.
     }
 
