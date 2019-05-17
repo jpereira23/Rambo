64dcae5345d8d5d5d83e9b75d0a5a71b48df1c7a
Jeff Pereira <jefferypereira3@gmail.com>
2019-05-16 18:53:13 -0700

    Tweaking on boarding a whole lot more

--- Rambo/OnBoardingViewController.swift
+++ Rambo/OnBoardingViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class OnBoardingViewController: UIViewController {
+class OnBoardingViewController: UIViewController, UIScrollViewDelegate {
     var headers: [String] = ["Welcome to Worthy", "Select a Template", "Add your Details", "Export and Apply to Jobs"]
     var subHeaders: [String] = ["Your convenient resume builder.", "Get started by choosing one of our \n job-worthy resume templates.", "Enter your unique qualifications through \nour simple prompts.", "Export your resume and kick start your job \nhunt more ready than before."]
     var imageNames: [String] = ["welcome.png", "select-template.png", "add-details.png", "export.png"]
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet var buildResume: UIButton!
+    var slides: [OnBoardingView] = []
     
     
     var aSlides: [OnBoardingView] = []
@@ -22,11 +23,12 @@ class OnBoardingViewController: UIViewController {
         super.viewDidLoad()
         
         aSlides = createSlides()
-        
-        setUpScrollView(slides: aSlides)
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        setUpScrollView(aSlides: aSlides)
         pageControl.numberOfPages = aSlides.count
         pageControl.currentPage = 0
-        pageControl.pageIndicatorTintColor = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        pageControl.pageIndicatorTintColor = UIColor(red: 235.0/255.0, green: 234.0/255.0, blue: 236.0/255.0, alpha: 1.0)
         pageControl.currentPageIndicatorTintColor = UIColor(red: 12.0/255.0, green: 199.0/255.0, blue: 156.0/255.0, alpha: 1.0)
         
         buildResume.layer.cornerRadius = 5
@@ -36,7 +38,6 @@ class OnBoardingViewController: UIViewController {
     }
     
     func createSlides() -> [OnBoardingView]{
-        var slides: [OnBoardingView] = []
         
         for count in 0...3{
             let slide1: OnBoardingView = Bundle.main.loadNibNamed("OnBoardingView", owner: self, options: nil)?.first as! OnBoardingView
@@ -49,15 +50,15 @@ class OnBoardingViewController: UIViewController {
         return slides
     }
     
-    func setUpScrollView(slides: [OnBoardingView]){
-        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
-        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+    func setUpScrollView(aSlides: [OnBoardingView]){
+        scrollView.frame = CGRect(x: 30, y: 145, width: 354, height: 429)
+        scrollView.contentSize = CGSize(width: 354 * CGFloat(aSlides.count), height: 429)
         scrollView.isPagingEnabled = true
         
         
-        for i in 0..<slides.count{
-            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
-            scrollView.addSubview(slides[i])
+        for i in 0..<aSlides.count{
+            aSlides[i].frame = CGRect(x: 354 * CGFloat(i), y: 0, width: 354, height: 429)
+            scrollView.addSubview(aSlides[i])
         }
     }
     
@@ -67,6 +68,53 @@ class OnBoardingViewController: UIViewController {
         vc.aChange = false
         self.present(vc, animated: true, completion: nil)
     }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/354)
+        pageControl.currentPage = Int(pageIndex)
+        
+        /*
+        
+        let maximumHorizontalOffset: CGFloat = scrollView.contentSize.width - scrollView.frame.width
+        let currentHorizontalOffset: CGFloat = scrollView.contentOffset.x
+        
+        let maximumVerticalOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
+        let currentVerticalOffset: CGFloat = scrollView.contentOffset.y
+        
+        let percentageHorizontalOffset: CGFloat = currentHorizontalOffset / maximumHorizontalOffset
+        let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
+        
+        let percentOffset: CGPoint = CGPoint(x: percentageHorizontalOffset, y: percentageVerticalOffset)
+        
+        if(percentOffset.x > 0 && percentOffset.x <= 0.25) {
+            
+            slides[0].imageView.transform = CGAffineTransform(scaleX: (0.25-percentOffset.x)/0.25, y: (0.25-percentOffset.x)/0.25)
+            slides[1].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.25, y: percentOffset.x/0.25)
+            
+        } else if(percentOffset.x > 0.25 && percentOffset.x <= 0.50) {
+            slides[1].imageView.transform = CGAffineTransform(scaleX: (0.50-percentOffset.x)/0.25, y: (0.50-percentOffset.x)/0.25)
+            slides[2].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.25, y: percentOffset.x/0.25)
+            
+        } else if(percentOffset.x > 0.50 && percentOffset.x <= 0.75) {
+            slides[2].imageView.transform = CGAffineTransform(scaleX: (0.75-percentOffset.x)/0.25, y: (0.75-percentOffset.x)/0.25)
+            slides[3].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.25, y: percentOffset.x/0.25)
+            
+        } else if(percentOffset.x > 0.75 && percentOffset.x <= 1) {
+            slides[3].imageView.transform = CGAffineTransform(scaleX: (1-percentOffset.x)/0.25, y: (1-percentOffset.x)/0.25)
+            //slides[4].imageView.transform = CGAffineTransform(scaleX: percentOffset.x, y: percentOffset.x)
+        }
+        */
+    }
+    
+    func scrollView(_ scrollView: UIScrollView, didScrollToPercentageOffset percentageHorizontalOffset: CGFloat) {
+        if(pageControl.currentPage == 0){
+            
+        }
+        
+    }
+    
+    
+    
     
     
 
