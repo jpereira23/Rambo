7bcd6cc9690663af96519ee4474dfe9df5317197
Jeffery Pereira <jefferypereira3@gmail.com>
2019-06-27 16:50:22 -0700

    Looking good!

--- Rambo/MainFlow/AddSchoolTableViewCell.swift
+++ Rambo/MainFlow/AddSchoolTableViewCell.swift
@@ -14,8 +14,8 @@ protocol AddSchoolTableViewCellDelegate{
 class AddSchoolTableViewCell: UITableViewCell, DateKeyboardDelegate {
 
     @IBOutlet weak var schoolName: UITextField!
-    @IBOutlet weak var degree: UITextField!
-    @IBOutlet weak var areaOfStudy: UITextField!
+    @IBOutlet weak var degree: SearchFieldClass!
+    @IBOutlet weak var areaOfStudy: SearchFieldClass!
     @IBOutlet weak var startDate: UITextField!
     @IBOutlet weak var endDate: UITextField!
     @IBOutlet weak var city: UITextField!
@@ -28,13 +28,19 @@ class AddSchoolTableViewCell: UITableViewCell, DateKeyboardDelegate {
     var index: Int!
     var isStart: Bool = false
     var isEnd: Bool = false
+    var degrees: [String] = ["Associate's Degree", "Bachelor's of Arts", "Bachelor's of Science", "Doctorate", "GED", "Masters"]
+    
+    var majors: [String] = ["Accounting", "Actuarial Sciences", "Advertising", "Agriculture", "Agricultural and Biological Engineering", "Agricultural Business Management", "Agriculture Economics", "Animal Bioscience", "Animal Sciences", "Anthropology", "Applied Mathematics", "Archaeology", "Architectual Engineering", "Architecture", "Art History", "Studio Art", "Art Education", "Bibehavioral Health", "Biochemistry", "Bioengineering", "Biology", "Biophysics", "Biotechnology", "Business Administration and Management", "Business Logistics", "Chemical Engineering", "Chemistry", "Children", "Civil Engineering", "Communications", "Computer Engineering", "Computer Science", "Crime, Law, and Justice", "Dance", "Earth Sciences", "Economics", "Electrical Engineering", "Elementary and Kindergarten Education", "Engineering Science", "English", "Environmental Systems Engineering", "Environmental Sciences", "Environmental Resource Management", "Film and Video", "Finance", "Food Science", "Forest Science", "Forest Technology", "General Science", "Geography", "Geosciences", "Graphic Design and Photography", "Health and Physical Education", "Health Policy and Administration", "History", "Horticulture", "Hotel, Restauarant and Institutional Management", "Human Development and Family Studies", "Industrial Engineering", "Information Sciences and Technology", "Journalism", "Kinesiology", "Landscape Architecture", "Law Enforcement and Correction", "Marine Biology", "Marketing", "Mathematics", "Mechanical Engineering", "Media Studies", "Meteorology", "Microbiology", "Mineral Economics", "Modern Languages", "Music Education", "Nuclear Engineering", "Nursing", "Nutrition", "Philosiphy", "Physics", "Physiology", "Political Science", "Pre-medicine", "Psychology", "Public Relations", "Real Estate", "Recreation and Parks", "Rehabilitation Services", "Religious Studies", "Secondary Education", "Sociology", "Social Work", "Special Education", "Speech Communication", "Speech Pathology and Audiology/Communication Disorder", "Statistics", "Telecommunications", "Theater", "Wildlife and Fishery Science", "Wildlife Technology", "Women's Studies"]
     override func awakeFromNib() {
         super.awakeFromNib()
         
         // Intialization code
+        
         isEmployee.isOn = true
         endDate.text = "Present"
         endDate.isEnabled = false
+        degree.dataList = degrees
+        areaOfStudy.dataList = majors
         
         startDate.addTarget(self, action: #selector(startDateSelected), for: .editingDidBegin)
         endDate.addTarget(self, action: #selector(endDateSelected), for: .editingDidBegin)
---
+++ Rambo/MainFlow/SearchFieldClass.swift
@@ -0,0 +1,142 @@
+//
+//  SearchFieldClass.swift
+//  Rambo
+//
+//  Created by Jeffery Pereira on 6/27/19.
+//  Copyright © 2019 Jeffery Pereira. All rights reserved.
+//
+
+import UIKit
+
+class SearchFieldClass: UITextField {
+
+    
+    var tableView: UITableView?
+    var resultList: [String] = []
+    var dataList: [String] = []
+    open override func willMove(toWindow newWindow: UIWindow?) {
+        super.willMove(toWindow: newWindow)
+        tableView?.removeFromSuperview()
+    }
+    
+    override open func willMove(toSuperview newSuperview: UIView?){
+        super.willMove(toSuperview: newSuperview)
+        
+        self.addTarget(self, action: #selector(SearchFieldClass.textFieldDidChange), for: .editingChanged)
+        self.addTarget(self, action: #selector(SearchFieldClass.textFieldDidBeginEditing), for: .editingDidBegin)
+        self.addTarget(self, action: #selector(SearchFieldClass.textFieldDidEndEditing), for: .editingDidEnd)
+        self.addTarget(self, action: #selector(SearchFieldClass.textFieldDidEndEditingOnExit), for: .editingDidEndOnExit)
+    }
+    
+    override open func layoutSubviews(){
+        super.layoutSubviews()
+        buildSearchTableView()
+    }
+    
+    @objc open func textFieldDidChange(){
+        filter()
+        updateSearchTableView()
+        tableView?.isHidden = false
+    }
+    
+    @objc open func textFieldDidBeginEditing(){
+        print("Begin Editing")
+    }
+    
+    @objc open func textFieldDidEndEditing(){
+        print("End Editing")
+    }
+    
+    @objc open func textFieldDidEndEditingOnExit(){
+        print("End on Exit")
+    }
+    
+    
+    fileprivate func filter(){
+        resultList = []
+        for i in 0..<dataList.count{
+            let theRange = (dataList[i] as NSString).range(of: text!, options: .caseInsensitive)
+            
+            if theRange.location != NSNotFound{
+                resultList.append(dataList[i])
+            }
+        }
+        tableView?.reloadData()
+    }
+
+}
+
+extension SearchFieldClass: UITableViewDelegate, UITableViewDataSource{
+    
+    func buildSearchTableView(){
+        if let tableView = tableView{
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CustomSearchTextFieldCell")
+            tableView.delegate = self
+            tableView.dataSource = self
+            self.window?.addSubview(tableView)
+        } else {
+            tableView = UITableView(frame: CGRect.zero)
+        }
+        
+        updateSearchTableView()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomSearchTextFieldCell", for: indexPath) as UITableViewCell
+        cell.backgroundColor = UIColor(red: 12.0/255.0, green: 199.0/255.0, blue: 156.0/255.0, alpha: 1.0)
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.text = resultList[indexPath.row]
+        return cell
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        self.text = resultList[indexPath.row]
+        tableView.isHidden = true
+        self.endEditing(true)
+    }
+    
+    
+    
+    func updateSearchTableView(){
+        if let tableView = tableView {
+            superview?.bringSubviewToFront(tableView)
+            var tableHeight: CGFloat = 0
+            tableHeight = tableView.contentSize.height
+            
+            if tableHeight < tableView.contentSize.height{
+                tableHeight -= 10
+            }
+            
+            var tableViewFrame = CGRect(x: 0, y: 0, width: frame.size.width - 4, height: tableHeight)
+            tableViewFrame.origin = self.convert(tableViewFrame.origin, to: nil)
+            tableViewFrame.origin.x += 2
+            tableViewFrame.origin.y += frame.size.height + 2
+            UIView.animate(withDuration: 0.2, animations: { [weak self] in
+                self?.tableView?.frame = tableViewFrame
+            })
+            
+            tableView.layer.masksToBounds = true
+            tableView.separatorInset = UIEdgeInsets.zero
+            tableView.layer.cornerRadius = 5.0
+            tableView.separatorColor = UIColor.white
+            tableView.backgroundColor = UIColor.white.withAlphaComponent(0.4)
+            
+            if self.isFirstResponder {
+                superview?.bringSubviewToFront(self)
+            }
+            
+            tableView.reloadData()
+        }
+        
+    }
+    
+    
+}
