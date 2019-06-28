1a54c6552e825fac5d74a540f3c986f7fa72a41b
Jeffery Pereira <jefferypereira3@gmail.com>
2019-06-28 02:25:08 -0700

    Still working on colleges

--- Rambo/MainFlow/AddSchoolTableViewCell.swift
+++ Rambo/MainFlow/AddSchoolTableViewCell.swift
@@ -11,9 +11,45 @@ import UIKit
 
 protocol AddSchoolTableViewCellDelegate{
 }
+
+struct SchoolStuff{
+    var dapipid: String
+    var opeId: String
+    var ipedsunitids: String
+    var locationName: String
+    var parentName: String
+    var parentdapipid: String
+    var locationtype: String
+    var address: String
+    var generalPhone: String
+    var adminName: String
+    var adminPhone: String
+    var adminEmail: String
+    var fax: String
+    var updateDate: String
+
+    init(dap: String, ope: String, iped: String, location: String, parent: String, parentDap: String, locationType: String, addresS: String, general: String, adminNamE: String, adminPhonE: String, adminEmaiL: String, faX: String, updateDatE: String){
+        dapipid = dap
+        opeId = ope
+        ipedsunitids = iped
+        locationName = location
+        parentName = parent
+        parentdapipid = parentDap
+        locationtype = locationType
+        address = addresS
+        generalPhone = general
+        adminName = adminNamE
+        adminPhone = adminPhonE
+        adminEmail = adminEmaiL
+        fax = faX
+        updateDate = updateDatE
+    }
+}
+
+
 class AddSchoolTableViewCell: UITableViewCell, DateKeyboardDelegate {
 
-    @IBOutlet weak var schoolName: UITextField!
+    @IBOutlet weak var schoolName: SearchFieldClass!
     @IBOutlet weak var degree: SearchFieldClass!
     @IBOutlet weak var areaOfStudy: SearchFieldClass!
     @IBOutlet weak var startDate: UITextField!
@@ -30,14 +66,21 @@ class AddSchoolTableViewCell: UITableViewCell, DateKeyboardDelegate {
     var isEnd: Bool = false
     var degrees: [String] = ["Associate's Degree", "Bachelor's of Arts", "Bachelor's of Science", "Doctorate", "GED", "Masters"]
     
-    var majors: [String] = ["Accounting", "Actuarial Sciences", "Advertising", "Agriculture", "Agricultural and Biological Engineering", "Agricultural Business Management", "Agriculture Economics", "Animal Bioscience", "Animal Sciences", "Anthropology", "Applied Mathematics", "Archaeology", "Architectual Engineering", "Architecture", "Art History", "Studio Art", "Art Education", "Bibehavioral Health", "Biochemistry", "Bioengineering", "Biology", "Biophysics", "Biotechnology", "Business Administration and Management", "Business Logistics", "Chemical Engineering", "Chemistry", "Children", "Civil Engineering", "Communications", "Computer Engineering", "Computer Science", "Crime, Law, and Justice", "Dance", "Earth Sciences", "Economics", "Electrical Engineering", "Elementary and Kindergarten Education", "Engineering Science", "English", "Environmental Systems Engineering", "Environmental Sciences", "Environmental Resource Management", "Film and Video", "Finance", "Food Science", "Forest Science", "Forest Technology", "General Science", "Geography", "Geosciences", "Graphic Design and Photography", "Health and Physical Education", "Health Policy and Administration", "History", "Horticulture", "Hotel, Restauarant and Institutional Management", "Human Development and Family Studies", "Industrial Engineering", "Information Sciences and Technology", "Journalism", "Kinesiology", "Landscape Architecture", "Law Enforcement and Correction", "Marine Biology", "Marketing", "Mathematics", "Mechanical Engineering", "Media Studies", "Meteorology", "Microbiology", "Mineral Economics", "Modern Languages", "Music Education", "Nuclear Engineering", "Nursing", "Nutrition", "Philosiphy", "Physics", "Physiology", "Political Science", "Pre-medicine", "Psychology", "Public Relations", "Real Estate", "Recreation and Parks", "Rehabilitation Services", "Religious Studies", "Secondary Education", "Sociology", "Social Work", "Special Education", "Speech Communication", "Speech Pathology and Audiology/Communication Disorder", "Statistics", "Telecommunications", "Theater", "Wildlife and Fishery Science", "Wildlife Technology", "Women's Studies"]
+    var majors: [String] = ["Accounting", "Actuarial Sciences", "Advertising", "Agriculture", "Agricultural and Biological Engineering", "Agricultural Business Management", "Agriculture Economics", "Animal Bioscience", "Animal Sciences", "Anthropology", "Applied Mathematics", "Archaeology", "Architectual Engineering", "Architecture", "Art History", "Studio Art", "Art Education", "Bibehavioral Health", "Biochemistry", "Bioengineering", "Biology", "Biophysics", "Biotechnology", "Business Administration and Management", "Business Logistics", "Chemical Engineering", "Chemistry", "Children", "Civil Engineering", "Communications", "Communication Studies", "Computer Engineering", "Computer Science", "Crime, Law, and Justice", "Dance", "Earth Sciences", "Economics", "Electrical Engineering", "Elementary and Kindergarten Education", "Engineering Science", "English", "Environmental Systems Engineering", "Environmental Sciences", "Environmental Resource Management", "Film and Video", "Finance", "Food Science", "Forest Science", "Forest Technology", "General Science", "Geography", "Geosciences", "Graphic Design and Photography", "Health and Physical Education", "Health Policy and Administration", "History", "Horticulture", "Hotel, Restauarant and Institutional Management", "Human Development and Family Studies", "Industrial Engineering", "Information Sciences and Technology", "Journalism", "Kinesiology", "Landscape Architecture", "Law Enforcement and Correction", "Marine Biology", "Marketing", "Mathematics", "Mechanical Engineering", "Media Studies", "Meteorology", "Microbiology", "Mineral Economics", "Modern Languages", "Music Education", "Nuclear Engineering", "Nursing", "Nutrition", "Philosiphy", "Physics", "Physiology", "Political Science", "Pre-medicine", "Psychology", "Public Relations", "Real Estate", "Recreation and Parks", "Rehabilitation Services", "Religious Studies", "Secondary Education", "Sociology", "Social Work", "Special Education", "Speech Communication", "Speech Pathology and Audiology/Communication Disorder", "Statistics", "Telecommunications", "Theater", "Wildlife and Fishery Science", "Wildlife Technology", "Women's Studies"]
+    var arrayOfSchools: [String] = []
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         // Intialization code
-        
+        do {
+            let theUrl = Bundle.main.path(forResource: "InstitutionCampus", ofType: "csv") as! String
+            let u = URL(fileURLWithPath: theUrl)
+            try processFile(at: u)
+        } catch {
+            print("error processing: \(error)")
+        }
         isEmployee.isOn = true
         endDate.text = "Present"
         endDate.isEnabled = false
@@ -75,6 +118,15 @@ class AddSchoolTableViewCell: UITableViewCell, DateKeyboardDelegate {
         }
     }
     func process(string: String) throws{
+        let stringOfWordsArray = try string.components(separatedBy: ",")
+        for i in 0..<stringOfWordsArray.count{
+            //NSLog("\(i%14): \(stringOfWordsArray[i])")
+            if (i%13) == 3{
+            
+                arrayOfSchools.append(stringOfWordsArray[i])
+            }
+        }
+        schoolName.dataList = arrayOfSchools
         
     }
     func processFile(at url: URL) throws{
