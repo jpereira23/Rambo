257e4ac72c767133ed810f965db592052ed739b2
Jeffery Pereira <jefferypereira3@gmail.com>
2019-08-02 17:17:50 -0700

    Objective working out now

--- Rambo/MainFlow/StepTwo.swift
+++ Rambo/MainFlow/StepTwo.swift
@@ -31,7 +31,27 @@ class StepTwo: UIView {
     
     override func awakeFromNib() {
         textView.placeholder = "I am a results-driven contributor who doesn’t mind working out the details to deliver quality work to clients."
+        addDoneButton()
         //add textview font-size and fon-family
+    }
+    
+    func addDoneButton(){
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+         done.tintColor = UIColor(displayP3Red: 13.0/255.0, green: 199.0/255.0, blue: 156.0/255.0, alpha: 1.0)
+        
+        let theItems = [flexSpace, done]
+        doneToolbar.items = theItems
+        doneToolbar.sizeToFit()
+        
+        textView.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction(){
+        textView.resignFirstResponder()
     }
     
     func checkUse() -> Bool{
